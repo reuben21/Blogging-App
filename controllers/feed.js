d5652729c295f51caf2a27b1b729b2d5a5da26e6
Reuben Coutinho <reuben211999@icloud.com>
2021-02-22 18:42:02 +0530

    implemented Posts Updated Feature

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,6 +1,7 @@
 const {validationResult} = require('express-validator');
 const Post = require('../models/post');
-
+const fs = require('fs');
+const path = require('path');
 
 exports.getPost = (req, res, next) => {
     Post.find().then(posts => {
@@ -83,3 +84,62 @@ exports.getSinglePost = (req, res, next) => {
     });
 
 };
+
+
+exports.updatePost = (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        const error = new Error('Validation Failed, Entered Data is incorrect');
+        error.statusCode = 422
+        throw error;
+    }
+
+    const post_id = req.params.postId;
+    const title = req.body.title;
+    const content = req.body.content;
+    let imageUrl = req.body.image;
+    console.log("WHAT IMAGE URL", imageUrl)
+    if (req.file) {
+        imageUrl = req.file.path;
+    }
+    if (!imageUrl) {
+        const error = new Error("No File Picked.");
+        error.statusCode = 422;
+        throw error;
+
+
+    }
+    Post.findById(post_id).then(post => {
+        if (!post) {
+            const error = new Error('Could Not Find The Post');
+            error.statusCode = 404;
+            throw error;
+        }
+        if (imageUrl !== post.imageUrl) {
+            clearImage(post.imageUrl);
+        }
+        post.title = title;
+        post.imageUrl = imageUrl;
+        post.content = content;
+        return post.save();
+
+
+    }).then(result => {
+        res.status(200).json({
+            message: 'Post Updated',
+            post: result
+        })
+    }).catch(err => {
+        if (!err.statusCode) {
+            err.statusCode = 500;
+        }
+        next(err);
+    });
+}
+
+const clearImage = filePath => {
+    filePath = path.join(__dirname, '..', filePath)
+    fs.unlink(filePath, err => {
+        console.log(err);
+    })
+}
--- routes/feed.js
+++ routes/feed.js
@@ -4,14 +4,21 @@ const feedController = require('../controllers/feed')
 
 const router = express.Router();
 
-router.get('/posts',feedController.getPost)
+router.get('/posts', feedController.getPost)
 
-router.post('/post',[
-    body('title').trim().isLength({min:5}),
-    body('content').trim().isLength({min:5}),
+router.post('/post', [
+    body('title').trim().isLength({min: 5}),
+    body('content').trim().isLength({min: 5}),
 
-],feedController.createPost)
+], feedController.createPost)
 
-router.get('/post/:postId',feedController.getSinglePost )
+router.get('/post/:postId', feedController.getSinglePost)
+
+
+router.put('/post/:postId', [
+    body('title').trim().isLength({min: 5}),
+    body('content').trim().isLength({min: 5}),
+
+], feedController.updatePost)
 
 module.exports = router;
