b28e6dc4808a340f228f0c12fe9b805a44ccee7a
reuben21 <reuben211999@icloud.com>
2021-03-04 11:28:52 +0530

    siging up and loggin in and adding the middleware

--- controllers/auth.js
+++ controllers/auth.js
@@ -36,3 +36,40 @@ exports.signup = (req, res, next) => {
         });
 };
 
+exports.login = (req, res, next) => {
+    const email = req.body.email;
+    const password = req.body.password;
+    let loadedUser;
+    User.findOne({ email: email })
+        .then(user => {
+            if (!user) {
+                const error = new Error('A user with this email could not be found.');
+                error.statusCode = 401;
+                throw error;
+            }
+            loadedUser = user;
+            return bcrypt.compare(password, user.password);
+        })
+        .then(isEqual => {
+            if (!isEqual) {
+                const error = new Error('Wrong password!');
+                error.statusCode = 401;
+                throw error;
+            }
+            const token = jwt.sign(
+                {
+                    email: loadedUser.email,
+                    userId: loadedUser._id.toString()
+                },
+                'somesupersecretsecret',
+                { expiresIn: '1h' }
+            );
+            res.status(200).json({ token: token, userId: loadedUser._id.toString() });
+        })
+        .catch(err => {
+            if (!err.statusCode) {
+                err.statusCode = 500;
+            }
+            next(err);
+        });
+};
