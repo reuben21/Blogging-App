1514bbec07ebff749e4da1732e4808437790a900
reuben21 <reuben211999@icloud.com>
2021-04-14 23:18:15 +0530

    updated frontend with socket connection

--- src/pages/Feed/Feed.js
+++ src/pages/Feed/Feed.js
@@ -1,5 +1,5 @@
 import React, {Component, Fragment} from 'react';
-
+import openSocket from 'socket.io-client';
 import Post from '../../components/Feed/Post/Post';
 import Button from '../../components/Button/Button';
 import FeedEdit from '../../components/Feed/FeedEdit/FeedEdit';
@@ -39,6 +39,7 @@ class Feed extends Component {
             .catch(this.catchError);
 
         this.loadPosts();
+        openSocket('http://localhost:4000')
     }
 
     loadPosts = direction => {
