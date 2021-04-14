1e3c1a9dd9abae66ba7776cf2019fadaffdf9811
reuben21 <reuben211999@icloud.com>
2021-04-14 23:29:16 +0530

    updated frontend with socket connection

--- src/pages/Feed/Feed.js
+++ src/pages/Feed/Feed.js
@@ -42,6 +42,23 @@ class Feed extends Component {
         openSocket('http://localhost:4000')
     }
 
+    addPost = post =>{
+        this.setState(prevState => {
+            const updatedPosts = [...prevState.posts];
+            if (prevState.postPage === 1) {
+                if (prevState.posts.length >= 2) {
+                    updatedPosts.pop();
+                }
+                updatedPosts.unshift(post);
+            }
+            return {
+                posts: updatedPosts,
+                totalPosts: prevState.totalPosts + 1
+            };
+        });
+
+    };
+
     loadPosts = direction => {
         if (direction) {
             this.setState({postsLoading: true, posts: []});
